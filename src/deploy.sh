ea9182b33e4b50e69cfdd585c7dbfa8ae34de30c
bigwin.eth <bitcoin@bigwin.eth>
2022-12-04 18:14:59 +0800

    compile before deploy

--- src/deploy.sh
+++ src/deploy.sh
@@ -5,7 +5,9 @@ aptos account fund-with-faucet --account dev3 --amount 1000000000
 aptos account fund-with-faucet --account sealib --amount 1000000000
 
 echo 'deploy sea_init/sea_lp ....'
-cd sea_init
+cd sea_lp
+aptos move compile --save-metadata
+cd ../sea_init
 aptos move publish --profile dev3 --assume-yes
 aptos move run --profile dev3 --assume-yes --function-id 0xf5de9e9d7a718c10964a8e5ce32de33c591979e2b2e76a1e58dcc9e6f74480df::spot_account::initialize_spot_account
 aptos move run --profile dev3 --assume-yes --function-id 0xf5de9e9d7a718c10964a8e5ce32de33c591979e2b2e76a1e58dcc9e6f74480df::spot_account::publish_pkg --args hex:"`xxd -ps -c10000000  /Users/guotie/guotie/chain/seaprotocol/members/bigwin/seaprotocol/src/sea_lp/build/SeaLP/package-metadata.bcs`"  hex:"`xxd -ps -c10000000  /Users/guotie/guotie/chain/seaprotocol/members/bigwin/seaprotocol/src/sea_lp/build/SeaLP/bytecode_modules/lp.mv`"
